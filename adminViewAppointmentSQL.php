7821f217d9e656b209e9daa6cd393c30c1e91430
Dec-246 <Declan.Clifford-Johnson@student.shu.ac.uk>
2024-05-03 21:19:34 +0100

    Admin can view appointments

---
+++ adminViewAppointmentSQL.php
@@ -0,0 +1,32 @@
+<?php
+// function getStaff()
+
+    // require_once('php/db_functions/db_connect.php');
+    
+    $sql = 'SELECT appointment.ID, appointment.dateTime, appointment.subject, 
+
+    CONCAT(patient.firstName," ",patient.lastName) AS "patientName",
+    CONCAT(staff.firstName," ",staff.lastName) AS "staffName"
+    FROM appointment
+
+    INNER JOIN patient ON (appointment.idPatient = patient.ID)
+    INNER JOIN staff ON (appointment.idDoctor = staff.ID)';
+
+$result = mysqli_query($conn, $sql);
+$appointment = array(); //Initialize the array
+while ($row = mysqli_fetch_assoc($result)) {
+
+    //while ($row = $result->fetch_assoc()) { //previous code for above
+    $appointment[] = $row; 
+}
+
+
+// $sql = 'SELECT employees.employee_id, CONCAT(employees.first_name," ",employees.last_name) AS "Name", 
+//             jobs.job_title, departments.department_name, 
+//             CONCAT(locations.street_address," ", locations.postal_code," ",locations.city," ",locations.state_province) AS "Location", 
+//             countries.country_name FROM employees 
+
+//             INNER JOIN departments ON (employees.department_id = departments.department_id) 
+//             INNER JOIN locations on (locations.location_id = departments.location_id) 
+//             INNER JOIN countries on (countries.country_id = locations.country_id) 
+//             INNER JOIN jobs ON (employees.job_id = jobs.job_id) order by employees.employee_id desc';
